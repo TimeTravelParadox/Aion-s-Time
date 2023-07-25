72b1fbef0a3dfd6722ece730d5f2e28b243eb5f3
LuisFontinelles <luisffontinelles@gmail.com>
2023-07-25 15:11:36 -0300

    tirar do inv 2

--- TimeTravelParadox/TimeTravelParadox/Scenes/Future/Future.swift
+++ TimeTravelParadox/TimeTravelParadox/Scenes/Future/Future.swift
@@ -1,126 +1,130 @@
 import SpriteKit
 
 class Future: SKNode, InventoryItemDelegate {
-
-  private let futureScene = SKScene(fileNamed: "FutureScene")
-  private var futureBG: SKSpriteNode?
-  private var mesa: SKSpriteNode?
-  private var monitorEsquerda: SKSpriteNode?
-  private var janela: SKSpriteNode?
-  
-  var computer: Computer?
-  var vault: Vault?
-  var hologram: Hologram?
     
-  let futureST = SKAction.repeatForever(SKAction.playSoundFileNamed("futureST.mp3", waitForCompletion: true))
-  
-  var delegate: ZoomProtocol?
-  
-  init(delegate: ZoomProtocol){
-    super.init()
-    self.delegate = delegate
-    self.computer = Computer(delegate: delegate)
-    self.vault = Vault(delegate: delegate)
-    self.hologram = Hologram(delegate: delegate)
-    vault?.setupCofre()
-    vault?.zPosition()
-    self.zPosition = 1
+    var pastScene: Past?
     
-    if let futureScene, let computer, let vault, let hologram {
-      futureBG = (futureScene.childNode(withName: "futureBG") as? SKSpriteNode)
-      futureBG?.removeFromParent()
-      mesa = futureScene.childNode(withName: "mesaFuturo") as? SKSpriteNode
-      mesa?.removeFromParent()
-      monitorEsquerda = futureScene.childNode(withName: "monitorEsquerda") as? SKSpriteNode
-      monitorEsquerda?.removeFromParent()
-      janela = futureScene.childNode(withName: "janela") as? SKSpriteNode
-      janela?.removeFromParent()
-      
-      self.isUserInteractionEnabled = true
- 
-      if let futureBG, let mesa, let monitorEsquerda, let janela{
-        self.addChild(mesa)
-        self.addChild(monitorEsquerda)
-        self.addChild(futureBG)
-        self.addChild(janela)
-      }
-      
-      self.addChild(computer)
-      computer.delegate = delegate
-      self.addChild(vault)
-      vault.delegate = delegate
-      self.addChild(hologram)
-      hologram.delegate = delegate
+    private let futureScene = SKScene(fileNamed: "FutureScene")
+    private var futureBG: SKSpriteNode?
+    private var mesa: SKSpriteNode?
+    private var monitorEsquerda: SKSpriteNode?
+    private var janela: SKSpriteNode?
+    
+    var computer: Computer?
+    var vault: Vault?
+    var hologram: Hologram?
+    
+    let futureST = SKAction.repeatForever(SKAction.playSoundFileNamed("futureST.mp3", waitForCompletion: true))
+    
+    var delegate: ZoomProtocol?
+    
+    init(delegate: ZoomProtocol, pastScene: Past){
+        super.init()
+        self.delegate = delegate
+        self.pastScene = pastScene
+        self.computer = Computer(delegate: delegate)
+        self.vault = Vault(delegate: delegate)
+        self.hologram = Hologram(delegate: delegate, delegateRemove: pastScene.clock!)
+        vault?.setupCofre()
+        vault?.zPosition()
+        self.zPosition = 1
         
-        computer.inventoryItemDelegate = self
-        vault.inventoryItemDelegate = self
-        hologram.inventoryItemDelegate = self
+        if let futureScene, let computer, let vault, let hologram {
+            futureBG = (futureScene.childNode(withName: "futureBG") as? SKSpriteNode)
+            futureBG?.removeFromParent()
+            mesa = futureScene.childNode(withName: "mesaFuturo") as? SKSpriteNode
+            mesa?.removeFromParent()
+            monitorEsquerda = futureScene.childNode(withName: "monitorEsquerda") as? SKSpriteNode
+            monitorEsquerda?.removeFromParent()
+            janela = futureScene.childNode(withName: "janela") as? SKSpriteNode
+            janela?.removeFromParent()
+            
+            self.isUserInteractionEnabled = true
+            
+            if let futureBG, let mesa, let monitorEsquerda, let janela{
+                self.addChild(mesa)
+                self.addChild(monitorEsquerda)
+                self.addChild(futureBG)
+                self.addChild(janela)
+            }
+            
+            self.addChild(computer)
+            computer.delegate = delegate
+            self.addChild(vault)
+            vault.delegate = delegate
+            self.addChild(hologram)
+            hologram.delegate = delegate
+            
+            computer.inventoryItemDelegate = self
+            vault.inventoryItemDelegate = self
+            hologram.inventoryItemDelegate = self
+            
+            
+        }
         
-      
     }
     
-  }
-  
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-  }
-  
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    guard let touch = touches.first else { return }
-    let location = touch.location(in: self)
-    let tappedNodes = nodes(at: location)
-    guard let tapped = tappedNodes.first else { return }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
     
-    switch tapped.name {
-    case "futureBG":
-      delegate?.zoom(isZoom: false, node: futureBG, ratio: 0)
-      print("futuro plano de fundo")
-        // Deselecionar o item
-        if HUD.shared.isSelected {
-            if HUD.shared.itemSelecionado != nil {
-                HUD.shared.removeBorder(from: HUD.shared.itemSelecionado!)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
+        guard let tapped = tappedNodes.first else { return }
+        
+        switch tapped.name {
+        case "futureBG":
+            delegate?.zoom(isZoom: false, node: futureBG, ratio: 0)
+            print("futuro plano de fundo")
+            // Deselecionar o item
+            if HUD.shared.isSelected {
+                if HUD.shared.itemSelecionado != nil {
+                    HUD.shared.removeBorder(from: HUD.shared.itemSelecionado!)
+                }
             }
-        }
-    case "itemDetail":
-        GameScene.shared.itemDetail?.interact()
-        return
-    case "crumpledPaper":
-        if let tapped = tapped as? SKSpriteNode { //isso eh um skspritenode?
-            select(node: tapped)
+        case "itemDetail":
+            GameScene.shared.itemDetail?.interact()
             return
-        }
-    case "mesaFuturo":
-      delegate?.zoom(isZoom: false, node: futureBG, ratio: 0)
-      print("futuro plano de fundo")
-        // Deselecionar o item
-        if HUD.shared.isSelected {
-            if HUD.shared.itemSelecionado != nil {
-                HUD.shared.removeBorder(from: HUD.shared.itemSelecionado!)
+        case "crumpledPaper":
+            if let tapped = tapped as? SKSpriteNode { //isso eh um skspritenode?
+                select(node: tapped)
+                return
             }
-        }
-    case "monitorEsquerda":
-      delegate?.zoom(isZoom: false, node: futureBG, ratio: 0)
-      print("futuro plano de fundo")
-        // Deselecionar o item
-        if HUD.shared.isSelected {
-            if HUD.shared.itemSelecionado != nil {
-                HUD.shared.removeBorder(from: HUD.shared.itemSelecionado!)
+        case "mesaFuturo":
+            delegate?.zoom(isZoom: false, node: futureBG, ratio: 0)
+            print("futuro plano de fundo")
+            // Deselecionar o item
+            if HUD.shared.isSelected {
+                if HUD.shared.itemSelecionado != nil {
+                    HUD.shared.removeBorder(from: HUD.shared.itemSelecionado!)
+                }
             }
-        }
-    case "janela":
-      delegate?.zoom(isZoom: false, node: futureBG, ratio: 0)
-      print("futuro plano de fundo")
-        // Deselecionar o item
-        if HUD.shared.isSelected {
-            if HUD.shared.itemSelecionado != nil {
-                HUD.shared.removeBorder(from: HUD.shared.itemSelecionado!)
+        case "monitorEsquerda":
+            delegate?.zoom(isZoom: false, node: futureBG, ratio: 0)
+            print("futuro plano de fundo")
+            // Deselecionar o item
+            if HUD.shared.isSelected {
+                if HUD.shared.itemSelecionado != nil {
+                    HUD.shared.removeBorder(from: HUD.shared.itemSelecionado!)
+                }
             }
+        case "janela":
+            delegate?.zoom(isZoom: false, node: futureBG, ratio: 0)
+            print("futuro plano de fundo")
+            // Deselecionar o item
+            if HUD.shared.isSelected {
+                if HUD.shared.itemSelecionado != nil {
+                    HUD.shared.removeBorder(from: HUD.shared.itemSelecionado!)
+                }
+            }
+        default:
+            break
         }
-    default:
-      break
+        clearItemDetail()
     }
-      clearItemDetail()
-  }
+    
     
     func clearItemDetail() {
         GameScene.shared.itemDetail?.removeFromParent()
@@ -135,5 +139,4 @@ class Future: SKNode, InventoryItemDelegate {
         GameScene.shared.itemDetail?.position = CGPoint(x: GameScene.shared.cameraPosition.x, y: GameScene.shared.cameraPosition.y)
         addChild(GameScene.shared.itemDetail!)
     }
-
 }

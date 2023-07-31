4e336782c8a3688e23483026ac3f8afe6b9e140b
mirellesine <mirellesine@gmail.com>
2023-07-31 10:41:50 -0300

    fps hidden

--- TimeTravelParadox/TimeTravelParadox/GameViewController.swift
+++ TimeTravelParadox/TimeTravelParadox/GameViewController.swift
@@ -31,8 +31,7 @@ class GameViewController: UIViewController {
       
       view.ignoresSiblingOrder = true
       
-      view.showsFPS = true
-      view.showsNodeCount = true
+      
     }
   }
   
